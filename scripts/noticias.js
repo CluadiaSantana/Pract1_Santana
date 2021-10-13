73a379f810bb7773b67f190e4c10c5af1e3176f8
CluadiaSantana <is726396@iteso.mx>
2021-10-13 13:26:54 -0500

    complete

--- scripts/noticias.js
+++
@@ -1,5 +0,0 @@
-export class News {
-    getAll(search) {
-        return axios.get(`https://newsapi.org/v2/everything?q=${search}&apiKey=20b0ee5352f449f6905044bbf1e35375`);
-    }
-}
--- src/scripts/noticias.ts
+++
@@ -1,8 +0,0 @@
-    
-   declare let axios
-    export class News{
-        
-        getAll(search: string){
-            return axios.get(`https://newsapi.org/v2/everything?q=${search}&apiKey=20b0ee5352f449f6905044bbf1e35375`);
-        }
-    }
